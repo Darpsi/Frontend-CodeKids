f1ae6f9e91906bb1763588d9bcb4c7954b010722
Nicolás Rodriguez <117177426+NiKolas1828@users.noreply.github.com>
2025-05-13 20:39:34 -0500

    adaptación home móvil

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -1,19 +1,25 @@
-import React from "react";
 import "../../assets/styles/home/Navbar.css";
+import { useState } from "react";
 
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <ul className="lista-nav">
-                <li><p className="codekids">Codekids</p></li>
-                <li><a className="links-nav" href="#section3">Servicios</a></li>
-            </ul>
-            <ul className="lista-nav">
-                <li><a className="links-nav" href="/login">Inicia Sesión</a></li>
-                <li><a className="links-nav-register" href="/register">Registrarse</a></li>
-            </ul>
-        </nav>
-    );
-};
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default Navbar;
+  return (
+    <nav className="navbar">
+      <ul className="lista-nav">
+        <li><p className="codekids">Codekids</p></li>
+        <li className="menu-toggle">
+          <button onClick={() => setMenuOpen(!menuOpen)} className="hamburger">
+            ☰
+          </button>
+        </li>
+      </ul>
+
+      <ul className={`lista-nav right-nav ${menuOpen ? "open" : ""}`}>
+        <li><a className="links-nav" href="#section3">Servicios</a></li>
+        <li><a className="links-nav" href="/login">Inicia Sesión</a></li>
+        <li><a className="links-nav-register" href="/register">Registrarse</a></li>
+      </ul>
+    </nav>
+  );
+}
