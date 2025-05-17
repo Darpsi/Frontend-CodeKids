573f5d2a374213f1aee49bc4e44d08f999942c6d
Nicolás Rodriguez <117177426+NiKolas1828@users.noreply.github.com>
2025-05-17 13:30:03 -0500

    contenido modulo 2 y 3

--- src/data/pyramid.js
+++ src/data/pyramid.js
@@ -10,7 +10,17 @@ export const pyramidData = [
     ]
   },
   {
-  moduleId: 2,
+    moduleId: 2,
+    niveles: [
+      { id: 1, label: "Variables", info: "Aprende a guardar información en tu programa.", ruta: "/modules/2/levels/1" },
+      { id: 2, label: "Operadores matemáticos", info: "Realiza cálculos con números.", ruta: "/modules/2/levels/2" },
+      { id: 3, label: "Operadores de comparación", info: "Compara valores y toma decisiones", ruta: "/modules/2/levels/3" },
+      { id: 4, label: "Comentarios", info: "Aprende a comentar tu código.", ruta: "/modules/2/levels/4" },
+      { id: 5, label: "Examen Final", info: "¡Demuestra lo aprendido!", ruta: "/exam/2" }
+    ]
+  },
+  {
+  moduleId: 3,
   niveles: [
     { id: 1, label: "Control de Flujo", info: "Aprende cómo toma decisiones un programa.", ruta: "/quiz/2" },
     { id: 2, label: "Condiciones y estructuras if-else", info: "Explora cómo ejecutar instrucciones según diferentes condiciones.", ruta: "/minigame/2/catcher" },
@@ -19,4 +29,13 @@ export const pyramidData = [
     { id: 5, label: "Examen Final", info: "¡Demuestra todo lo que has aprendido sobre control de flujo!", ruta: "/exam/2" },
     ]
   },
+  {
+    moduleId: 4,
+    niveles: [
+      { id: 1, label: "Funciones", info: "Aprende a crear funciones para organizar tu código.", ruta: "/quiz/3" },
+      { id: 2, label: "Ámbito de variables", info: "Descubre cómo las variables pueden ser locales o globales.", ruta: "/minigame/3/maze" },
+      { id: 3, label: "Funciones recursivas", info: "Entiende cómo una función puede llamarse a sí misma.", ruta: "/minigame/3/sorter" },
+      { id: 4, label: "Examen Final", info: "¡Demuestra todo lo que has aprendido sobre funciones!", ruta: "/exam/3" }
+    ]
+  }
 ];
